fb55a118398b0cf0273e32866e836023c1874c75
stupidprogrammer1 <stupidprogrammer1@gmail.com>
2016-08-07 16:06:23 +0530

    adding the main file
    you can execute this file

---
+++ main.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2016 Sarcasm Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Stupidity License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.idontgiveashit.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import godHelpUs
+import pleaseGod
+
+
+# This is where it all begins
+from rj import Rj
+from rj import chandamama
+from rj import eclipse
+from rj import grahanam
+from rj import stupidity
+from rj import sarcasm
+from rj import rp
+
+# Sometimes you might get import errors here
+from rp import Rp
+from rp import english
+from rp import grammar
+from rp import butter
+from rp import soap
+
+from allofus import whatTheFuk
+
+
+class TheBestRjs(Rj,Rp):
+	'''
+		Class
+	'''
+	def __init__(self):
+		print "The jokes are coming!"
+		print "Create an object of the class TheBestRjs"\
+			" and just run the functions to see the output"
+
+	def kill_associate(self, die=True):
+	# You will die after reading this.
+	# Add something only if it kills after reading.
+		pass
+	    
+	def unpossible_to_understand(self, ):
+	# Really unpossible to understand these.
+	# Add here if you understand after 5-10 mins.
+		pass
+	
+	def the_joke_the_jokes(self):
+	# The 'the' jokes are here the.
+	# Add only 'the' jokes here the.
+		pass
+	
+	def lame_ones(self):
+	# Lamest of the lamest ones are here.
+	# Add only if it is lame.
+		pass
+	
+	def three_mistakes_per_sentance_frequency(self, frequency=3):
+	# Yes, This one belongs to rp.
+	# This section contains all the sentances with atleast 3 mistakes in it.
+	# Add only if the mistakes greater than or equal to the frequency
+        # and if it belongs to rp.
+		pass
+	
+	def tricky_riddles(self):
+	# Let's see if you'll be able to solve these riddles.
+	# Add if the sentances are like riddles.
+		print "If we are doing , atleaset write the reason why we are not doing. --> rp"
+
+TheBestRjs()
