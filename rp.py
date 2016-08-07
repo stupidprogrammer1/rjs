d32e4f511c75d271096f9853b9fd6127e9252304
stupidprogrammer1 <stupidprogrammer1@gmail.com>
2016-08-07 16:05:28 +0530

    adding the rp file

---
+++ rp.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2016 Sarcasm Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Stupidity License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.idontgiveashit.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+#Notice - Generally this function should be empty as rp's mind... sry no mind..
+
+
+class Rp(object):
+    pass
+
+def english():
+# This is a mental condition
+    pass
+
+def grammar():
+# This is a mental condition
+    pass
+
+def butter():
+# It's a feature unfortunately
+    pass
+
+def soap():
+# It's a feature unfortunately
+    pass
