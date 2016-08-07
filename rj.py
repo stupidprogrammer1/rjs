6f7ea65b04f7af8a280be80660535cccba9cdf06
stupidprogrammer1 <stupidprogrammer1@gmail.com>
2016-08-07 16:04:36 +0530

    adding the rj file
    hah!

---
+++ rj.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2016 Sarcasm Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Stupidity License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.idontgiveashit.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Rj(object):
+    pass
+
+def chandamama():
+# He is also known as chandamama
+    pass
+
+def eclipse():
+# He is also known as eclipse
+    pass
+
+def grahanam():
+# grahanam == eclipse in telugu 
+    pass
+
+def stupidity():
+# It's a feature
+    pass
+
+def sarcasm():
+# It's a feature
+    pass
+
+def rp():
+# His heir
+    pass
