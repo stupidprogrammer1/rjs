8d1b9d15f5e7eb25a0222db973b49ac7ee24b6e6
stupidprogrammer1 <stupidprogrammer1@gmail.com>
2016-08-07 18:15:06 +0530

    Added jokes in all respective methods
    Add jokes as far as I remmeber

--- main.py
+++ main.py
@@ -48,33 +48,224 @@ class TheBestRjs(Rj,Rp):
 	def kill_associate(self, die=True):
 	# You will die after reading this.
 	# Add something only if it kills after reading.
-		pass
-	    
+		print """
+			XYZ: Hi RS My name is XYZ and I'm from electronics backgroud.
+			RS: Ohh, Is it? But I dont see any electrons in your background.
+			XYZ: -_-
+		"""
+	    	print """
+			XYZ: So, RS this will be the output at the end of the day.
+			RS: Ohh, mari morning em ostadi?
+			XYZ: -_-
+		"""
+
 	def unpossible_to_understand(self, ):
 	# Really unpossible to understand these.
 	# Add here if you understand after 5-10 mins.
 		pass
 	
-	def the_joke_the_jokes(self):
-	# The 'the' jokes are here the.
-	# Add only 'the' jokes here the.
-		pass
-	
 	def lame_ones(self):
 	# Lamest of the lamest ones are here.
 	# Add only if it is lame.
-		pass
-	
+ 	   	print """
+			XYZ: Call chesdam 
+			RS: Enduku Hand cheyalema?
+			XYZ: -_-
+		"""
+ 	   	print """
+			XYZ: Manually run chesdam adi..
+			RS: Enduku Womanly cheyalema?
+			XYZ: -_-
+		"""
+
+	def just_rjs_rps(self):
+	# Put simple rjs/rps  ones are here.
+	# Add only if it is lame.
+ 	   	print """
+	 	   	RS: Adi push chesara?
+			XYZ: Ha chesam
+			RS: Edi?
+			XYZ: Ade
+			RS: Edi?
+			XYZ: Ade
+			RS: Okay.
+			XYZ: :D
+		"""
+		print """
+			RP: Code-wise everthing seems good but, I won't merge unitil tempest are good
+			XYZ: Okay, What is the branch for tempest.
+			RP: I don't know.
+			XYZ: -_-
+		"""
+
 	def three_mistakes_per_sentance_frequency(self, frequency=3):
 	# Yes, This one belongs to rp.
 	# This section contains all the sentances with atleast 3 mistakes in it.
 	# Add only if the mistakes greater than or equal to the frequency
         # and if it belongs to rp.
-		pass
-	
+		print """
+			(Mail)
+			Good Morning, As you are also in the mail loop could you please increase the priority of this.
+		"""
+		
+		print """
+			(Mail)
+			TCS associates needs to following a dress code. Its also each ones responsibility to make maintain dress code among our colleagues.
+			One of your team mate, XYZ has been very frequently wearing jeans in the Non Friday working days.
+			This is in opposition to our dress code rules. 
+			If somebody sees { TUV-DAS or any other higher official } , it not only an embarrassment for our team both also for entire  Cloud project and ericsson. 
+			Last time he has been cautioned by me , not to wear it. But still he is coming to office  by wearing jeans. 
+			Hope you take a action, or forward this mail to RS. 
+			If I see next time, I am going to forward this mail to RS. 
+		"""
+		print """
+			(ping)
+			XYZ askes some question
+			RP: It's is uneven from me to understand
+		"""
+		print """
+			(ping)
+			XYZ: So how's Hungary?
+			RP: No water problem
+			XYZ: (lolololol)
+			RP: Sun shuts down early.
+			XYZ: -_-
+		"""
+
+	def the_joke_the_jokes(self):
+	# The 'the' jokes are here the.
+	# Add only 'the' jokes here the.
+		print """
+			(ping)
+			RP: What is the number of the ABC?
+			XYZ: -__-
+		"""
+		print """
+			(mail)
+			RP: Hi, Can you book the ABC for one day?
+			XYZ: -__- (howle, room aa ra adi book cheyadaniki)
+		"""
+		print """
+			(ping)
+			RP: Is the PQR available?
+			XYZ: -_- (Enduku ra vadini kuda book cheskuntava?)
+		"""
+
 	def tricky_riddles(self):
 	# Let's see if you'll be able to solve these riddles.
 	# Add if the sentances are like riddles.
 		print "If we are doing , atleaset write the reason why we are not doing. --> rp"
+		print """
+			XYZ: I asked ppl to come for an interview tomorrow.
+			RS: I won't mind even if you have called them for an interview yesterday.
+			XYZ: ?
+		"""
 
+	def classy_af(self):
+	# This part is for all my fellow programmers who like classic programming jokes
+	# yes, pjs.
+	# These are really cool jokes you'll need to know programming to understand this.
+	# If you're rs or rp don't scroll down.. you'll anyways not understand these :P
+		print """
+			Programmer(noun.)
+			A person who fixed a problem that you don't know you have, in a way you don't understand.
+		"""
+		print """
+			Algorithm(noun.)
+			Word used by programmers when...
+			they do not want to explain what they did. ;)
+		"""
+		print """
+			['hip', 'hip']
+			
+			get it? NO?
+			Hip..Hip..Array!!!
+		"""
+		print """
+			#wife {
+				right: 100%;
+				margin: 0;
+			}
+		"""
+		print """
+			Q. Why do JAVA developers wear glasses??
+			A. Because they can't c#
+		"""
+		print """
+			There are 10 kinds of people in this world.
+			Those who understand binary, those who don’t.
+		"""
+		print """
+			Q. What is a programmer's favourite hangout place?
+			A. Foo Bar
+		"""
+		print """
+			If at first you don’t succeed; call it version 1.0.
+		"""
+		print """
+			Keyboard not found.....
+			Press F1 to continue
+		"""
+		print """
+			A Foo walks into a bar, takes a look around and says..
+			'Hello World!'
+		"""
+		print """
+			Hide and seek champion..
+			;
+			since 1958
+		"""
+		print """
+			Why is it that women find C to be more attractive than Java?
+ 			Because C doesn’t treat them like objects.
+		"""
+		print """
+			I don't see women as 'objects'.
+			I consider each to be in 'class' of her own.
+		"""
+		print """
+			A programmer got stuck in the shower because the instructions on shampoo
+			bottle said...
+			
+			lather, rinse, repeat.
+		"""
+		print """
+			In order to understand recursion you must first understand recursion.
+		"""
+		print """
+			Q: "Why are object-oriented programming languages wealthy?"
+			A: Inheritance
+ 		"""
+ 		print """
+ 			ASCII stupid question, get a stupid ANSI
+ 		"""
+ 		print """
+ 			http://www.google.com/search?hl=en&q=recursion
+ 		"""
+ 		print """
+ 			Husband:  "Make me a sandwich"
+ 			Wife: "No, Make it yourself"
+ 			Husband: "sudo Make me a sandwich"
+ 			Wife: "Okay"
+ 		"""
+ 		print """
+ 			Husband:  "Make me a sandwich!"
+			Wife: "No, Make it yourself"
+			Husband:  "sudo Make me a sandwich"
+			Wife:  "You are not in sudoers list. The incident will be reported."
+ 		"""
+		print """
+			Q: how many programmers does it take to change a light bulb?
+			A: None, that's a hardware problem
+		"""
+		print """
+			A group of DBAs walk into a resraurant.
+ 			One waved over the waiter: “Can we join these tables?”.
+ 		"""
+ 		print """
+ 			In case of FIRE..
+ 			1. git commit
+ 			2. git push
+ 			3. leave building
+ 		"""
 TheBestRjs()
